5b6f8ccf1913c59b3ff08a726d70cc4455c3b52a
RiqueDev18 <128099120+RiqueDev18@users.noreply.github.com>
2023-03-16 18:58:51 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box
+
+function setup() {
+createCanvas(400,400);
+box = createSprite (200,200,20,20);
+}
+
+function draw() 
+{
+background(30);
+drawSprites();
+if (keyDown(LEFT_ARROW)) {
+box.position.x = box.position.x -4
+}
+}
+
+
+
+
